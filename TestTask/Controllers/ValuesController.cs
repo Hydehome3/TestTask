6b9c93054cee75e77bfd9fa11faf3ba1a2861f5c
hydehome3 <hydehome3@gmail.com>
2019-12-02 14:48:41 +0300

    Доделать тест поиска и контроллер

--- TestTask/Controllers/ValuesController.cs
+++ TestTask/Controllers/ValuesController.cs
@@ -3,43 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace TestTask.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ValuesController : Controller
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ContentResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var client = new RestClient("https://docs.microsoft.com/api/");
+            var request = new RestRequest("search?search=LINQ&locale=ru-ru&facet=category&%24skip=0&%24top=5");
+
+            var queryResult = client.Execute(request);
+
+            return Content(queryResult.Content);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
---
+++ UnitTestApp.Tests/Deserialization/RootObject.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestApp.Tests.Deserialization
+{
+    class RootObject
+    {
+        [JsonProperty("results")]
+        public Result[] Results { get; set; }
+    }
+
+    class Result
+    {
+        [JsonProperty("descriptions")]
+        public Description[] Description { get; set; }
+    }
+
+    class Description
+    {
+        [JsonProperty("content")]
+        public string Content { get; set; }
+    }
+}
